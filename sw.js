a518ec7f900d87c798833609cb3b9c4174596ac8
dmitryu81-ctrl <dmitryu81@gmail.com>
2026-09-19 17:13:01 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='moe-pitanie-2026-09-14-v1';
+const CACHE='moe-pitanie-2026-09-21-v1';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./history.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('moe-pitanie-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
